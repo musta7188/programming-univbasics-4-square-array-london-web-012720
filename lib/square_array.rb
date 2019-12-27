02ee2ab08bf0ba5806b161b2fa6ceb17ccdbc922
Moustafa Mohamed <musta7188@hotmail.it>
2019-12-27 21:55:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,13 +4,10 @@ def square_array(array)
 
   counter = 0
 
-  x = 2
 
- y = array[counter]
- 
 while array.size > index do
 
-new_array.push( y ** x)
+new_array <<  array[counter] ** x)
 
 counter += 1
 
